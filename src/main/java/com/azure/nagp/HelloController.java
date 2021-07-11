843d32c87c18dfd976342bd30827e3a61840e55d
HimanshiVerma05 <iamtherockstar05@gmail.com>
2021-07-11 23:45:16 +0530

    changed hellp controller

--- src/main/java/com/azure/nagp/HelloController.java
+++ src/main/java/com/azure/nagp/HelloController.java
@@ -15,22 +15,17 @@ import java.sql.ResultSet;
 import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
-import com.azure.storage.*;
 import com.azure.storage.blob.*;
-import com.azure.storage.blob.models.BlobErrorCode;
 import com.azure.storage.blob.models.BlobItem;
-import com.azure.storage.blob.models.BlobStorageException;
 import com.azure.storage.blob.specialized.BlockBlobClient;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import com.microsoft.sqlserver.jdbc.SQLServerDriver;
 
 @RestController
 public class HelloController {
